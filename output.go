bece9c6a1b3163b68936489eab09ae8a86bca542
Liberbell <liberbell@gmail.com>
2019-07-12 16:12:58 +0900

    output2

--- output.go
+++ output.go
@@ -6,6 +6,8 @@ func main() {
 	str1 := "The quick red fox"
 	str2 := "jumped over"
 	str3 := "the lazy brown dog."
+	aNumber := 42
+	isTrue := true
 
 	stringLength, err := fmt.Println(str1, str2, str3)
 	if err == nil {
