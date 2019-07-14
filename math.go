c2e1ab67de22884b76ad32d5d84ad22801d9c616
Liberbell <liberbell@gmail.com>
2019-07-14 17:27:36 +0900

    math3

--- math.go
+++ math.go
@@ -6,4 +6,8 @@ func main() {
 	i1, i2, i3 := 12, 45, 68
 	intsum := i1 + i2 + i3
 	fmt.Println("Integer sum: ", intsum)
+
+	f1, f2, f3 := 23.5, 65.1, 76.3
+	floatsum := f1 + f2 + f3
+	fmt.Println("Float sum: ", floatsum)
 }
