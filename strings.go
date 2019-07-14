4b9376eaf92783f5feae1372d267ebd9b56cda5b
Liberbell <liberbell@gmail.com>
2019-07-14 17:12:02 +0900

    strings6

--- strings.go
+++ strings.go
@@ -7,10 +7,12 @@ import (
 
 func main() {
 	str1 := "An implicitly typed string"
-	fmt.Printf("str1: %v:%T\n", str1, str1)
+	fmt.Printf("str1: %v :%T\n", str1, str1)
 
 	str2 := "An explicitly typed string"
-	fmt.Printf("str2: %v:%T\n", str2, str2)
+	fmt.Printf("str2: %v :%T\n", str2, str2)
 
 	fmt.Println(strings.ToUpper(str1))
+	fmt.Println(strings.Title(str1))
+
 }
