34726e3e8d5c57c635076863f2a5f0a367bb2d0e
Liberbell <liberbell@gmail.com>
2019-07-16 07:41:23 +0900

    slices6

--- slices.go
+++ slices.go
@@ -6,6 +6,6 @@ func main() {
 	var colors = []string{"Red", "Green", "Blue"}
 	fmt.Println(colors)
 
-	colors.Append(colors, "Purple")
+	colors = colors.Append(colors, "Purple")
 	fmt.Println(colors)
 }
