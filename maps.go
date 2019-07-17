be9d28b1aacbcd9b07916fca63961afd9fbeabaa
Liberbell <liberbell@gmail.com>
2019-07-17 09:08:05 +0900

    maps3

--- maps.go
+++ maps.go
@@ -5,4 +5,9 @@ import "fmt"
 func main() {
 	states := make(map[string]string)
 	fmt.Println(states)
+
+	states["WA"] = "Washington"
+	states["OR"] = "Oregon"
+	states["CA"] = "California"
+	fmt.Println(states)
 }
