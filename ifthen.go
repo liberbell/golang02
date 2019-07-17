5356f63d335d2008f5e58b028eb0cd7de41023a4
Liberbell <liberbell@gmail.com>
2019-07-18 07:41:48 +0900

    ifthen3

--- ifthen.go
+++ ifthen.go
@@ -1,6 +1,15 @@
 package main
 
+import "fmt"
+
 func main() {
 	var x float64 = 42
 	var result string
+
+	if x < 0 {
+		resutl = "Less than zero"
+	} else {
+		result = "Greater than or equal to Zero"
+	}
+	fmt.Println(result)
 }
