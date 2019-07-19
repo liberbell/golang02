51fedc3ad83ebdc8f2fc5bc0ca105126d380efeb
Liberbell <liberbell@gmail.com>
2019-07-19 09:34:10 +0900

    ifthen1

--- ifthen.go
+++ ifthen.go
@@ -6,7 +6,7 @@ func main() {
 	// var x float64 = 0
 	var result string
 
-	if x := 42; x < 0 {
+	if x := -42; x < 0 {
 		result = "Less than Zero"
 	} else if x == 0 {
 		result = "Equal to Zero"
@@ -14,4 +14,6 @@ func main() {
 		result = "Greater than Zero"
 	}
 	fmt.Println("Result: ", result)
+
+	fmt.Println("Value of x: ", x)
 }
