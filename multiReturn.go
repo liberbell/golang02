d9a54035712a28cdee2db64fd07361fff2fc75a0
Liberbell <liberbell@gmail.com>
2019-07-20 14:09:20 +0900

    multiReturn2

---
+++ multiReturn.go
@@ -0,0 +1,11 @@
+package main
+
+func main() {
+	n1, l1 := FullName("Zaikah", "Brownshed")
+}
+
+func FullName(f, l string) (string, int) {
+	full := f + " " + l
+	lenght := len(full)
+	return full, length
+}
