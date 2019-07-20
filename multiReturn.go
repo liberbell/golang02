91bfa06a60bd2928527739b939b3afd053166354
Liberbell <liberbell@gmail.com>
2019-07-20 14:10:55 +0900

    multiReturn3

--- multiReturn.go
+++ multiReturn.go
@@ -1,7 +1,10 @@
 package main
 
+import "fmt"
+
 func main() {
 	n1, l1 := FullName("Zaikah", "Brownshed")
+	fmt.Printf("Fullname: %v, Number of chars: %v%", n1, l1)
 }
 
 func FullName(f, l string) (string, int) {
