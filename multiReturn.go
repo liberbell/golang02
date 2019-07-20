26746702af64f7b5d30ec651a59ac42c95e42e93
Liberbell <liberbell@gmail.com>
2019-07-20 14:13:42 +0900

    multiReturn6

--- multiReturn.go
+++ multiReturn.go
@@ -12,3 +12,9 @@ func FullName(f, l string) (string, int) {
 	length := len(full)
 	return full, length
 }
+
+func FullNameNakedReturn(f, l string) (full string, length int) {
+	full := f + " " + l
+	length := len(full)
+	return
+}
