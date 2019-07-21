571e7922512dc813f2b9c890b911aa203171c423
Liberbell <liberbell@gmail.com>
2019-07-21 16:04:57 +0900

    interfaces8

--- interfaces.go
+++ interfaces.go
@@ -13,7 +13,26 @@ func (d Dog) Speak() string {
 	return "woof!"
 }
 
+type Cat struct {
+}
+
+func (c Cat) Speak() string {
+	return "Meow!"
+}
+
+type Cow struct {
+}
+
+func (c Cow) Speak() string {
+	return "Moo!"
+}
+
 func main() {
 	poodle := Animal(Dog{})
 	fmt.Println(poodle)
+
+	animals := []Animal{Dog{}, Cat{}, Cow{}}
+	for _, animal := range animals {
+		fmt.Println(animal.Speak())
+	}
 }
