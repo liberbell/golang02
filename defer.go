e8a38c5ed0ab816d323df408fda55e80f431e220
Liberbell <liberbell@gmail.com>
2019-07-22 07:50:13 +0900

    defer3

--- defer.go
+++ defer.go
@@ -3,6 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Close the file.")
+	defer fmt.Println("Close the file.")
 	fmt.Println("Open the file.")
+
+  defer fmt.Println("Statement 1.")
+  defer fmt.Println("Statement 2.")
+  defer fmt.Println("Statement 3.")
+  defer fmt.Println("Statement 4.")
+  fmt.Println("Undefer Statement.")
 }
+
+func
