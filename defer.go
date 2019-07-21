69837f32de4ab880ebbbcb7d75e65595d5a6c125
Liberbell <liberbell@gmail.com>
2019-07-22 07:52:37 +0900

    defer4

--- defer.go
+++ defer.go
@@ -6,11 +6,18 @@ func main() {
 	defer fmt.Println("Close the file.")
 	fmt.Println("Open the file.")
 
-  defer fmt.Println("Statement 1.")
-  defer fmt.Println("Statement 2.")
-  defer fmt.Println("Statement 3.")
-  defer fmt.Println("Statement 4.")
-  fmt.Println("Undefer Statement.")
+	defer fmt.Println("Statement 1.")
+	defer fmt.Println("Statement 2.")
+
+	myFunc()
+
+	defer fmt.Println("Statement 3.")
+	defer fmt.Println("Statement 4.")
+	fmt.Println("Undefer Statement.")
 }
 
-func
+func myFunc() {
+	defer fmt.Println("Defered in the function.")
+	fmt.Println("Undefered in the function.")
+
+}
