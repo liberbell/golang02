c7d402b35921238b2ae8311b0b420807baba4f88
Liberbell <liberbell@gmail.com>
2019-07-24 08:36:40 +0900

    walkdirs3

--- walkdirs.go
+++ walkdirs.go
@@ -8,4 +8,24 @@ import (
 func main() {
 	root, _ := filepath.Abs(".")
 	fmt.Println("processing path", root)
+
+  err := filepath.Walk(root, processPath)
+  if err != nil {
+    fmt.Println("error: ", err)
+  }
+}
+
+func processPath(path string, info os.FileInfo, err error) {
+  if err != nil {
+    retrun err
+  }
+
+  if path != "." {
+    if info.isDir() {
+      fmt.Println("Directory: ", path)
+    } else {
+      fmt.Println("File: ", path)
+    }
+  }
+  return nil
 }
