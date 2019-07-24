4a0e648f900c32cac5592b5ffe44363f9f3472d8
Liberbell <liberbell@gmail.com>
2019-07-25 08:17:11 +0900

    webreq4

--- webreq.go
+++ webreq.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 func main() {
 	url := "http://services.explorecalifornia.org/json/tours.php"
+
+	resp, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Response Type: %T", resp)
 }
