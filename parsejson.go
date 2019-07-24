f9d4b682c372e9e16d7b1e1553dc917342be246c
Liberbell <liberbell@gmail.com>
2019-07-25 08:24:22 +0900

    parsejson4

--- parsejson.go
+++ parsejson.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
 	url := "http://services.explorecalifornia.org/json/tours.php"
@@ -13,4 +16,9 @@ func checkError(err error) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func contentFromServer(url string) string {
+	resp, err := http.Get(url)
+	checkError(err)
 }
